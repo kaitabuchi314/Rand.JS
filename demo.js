ddf899a5ce51945c72295c39ee0e1b442fd9d978
kaitabuchi <93952418+thecodingcookery@users.noreply.github.com>
2023-05-11 23:40:41 +0000

    Currently, P5.JS is not needed for demo.

--- demo.js
+++ demo.js
@@ -1,10 +1,10 @@
 let randjs;
 
 function setup() {
-    createCanvas(400, 400);
+    //createCanvas(400, 400);
 
     randjs = new rand();
-    background(map(randjs.pseudoSeeded(353636)*100, 0, 30, 0, 255)*10);
+    //background(map(randjs.pseudoSeeded(353636)*100, 0, 30, 0, 255)*10);
 }
 
 function draw() {
